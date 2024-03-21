393d2a171e7a28c1a77280794fe8268dbe9ea16e
Valik <valik3201@gmail.com>
2024-03-21 17:19:37 +0100

    refactor: update code with try..catch and async/await as per Node.js documentation

--- contacts.js
+++ contacts.js
@@ -4,84 +4,85 @@ const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.resolve("./db/contacts.json");
 
-listContacts = () => {
-  fs.readFile(contactsPath)
-    .then((data) => console.table(JSON.parse(data)))
-    .catch((err) => console.error(err.message));
+listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    console.table(JSON.parse(data));
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
-getContactById = (contactId) => {
-  fs.readFile(contactsPath)
-    .then((data) => {
-      const contacts = JSON.parse(data);
-      const contactById = contacts.find((contact) => contact.id === contactId);
+getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contactById = contacts.find((contact) => contact.id === contactId);
 
-      if (contactById) {
-        console.table(contactById);
-        return contactById;
-      } else {
-        console.warn("\x1b[31m", "Contact with the specified ID not found");
-        return null;
-      }
-    })
-    .catch((err) => console.error(err.message));
+    if (contactById) {
+      console.table(contactById);
+      return contactById;
+    } else {
+      console.warn("\x1b[31m", "Contact with the specified ID not found");
+      return null;
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
-removeContact = (contactId) => {
-  fs.readFile(contactsPath)
-    .then((data) => {
-      const contacts = JSON.parse(data);
-      const contactIndex = contacts.findIndex(
-        (contact) => contact.id === contactId
-      );
+removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contactIndex = contacts.findIndex(
+      (contact) => contact.id === contactId
+    );
 
-      if (contactIndex !== -1) {
-        const removedContact = contacts.splice(contactIndex, 1)[0];
+    if (contactIndex !== -1) {
+      const removedContact = contacts.splice(contactIndex, 1)[0];
+      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-        fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-
-        console.table(removedContact);
-        console.log(
-          "\x1b[32m",
-          "Contact",
-          removedContact.name,
-          "deleted successfully!"
-        );
-
-        return removedContact;
-      } else {
-        console.warn("\x1b[31m", "Contact with the specified ID not found");
-        return null;
-      }
-    })
-    .catch((err) => console.error(err.message));
-};
-
-addContact = (name, email, phone) => {
-  fs.readFile(contactsPath)
-    .then((data) => {
-      const contacts = JSON.parse(data);
-
-      const newContact = {
-        id: uuidv4(),
-        name,
-        email,
-        phone,
-      };
-
-      contacts.push(newContact);
-
-      fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-      console.table(newContact);
+      console.table(removedContact);
       console.log(
         "\x1b[32m",
         "Contact",
-        newContact.name,
-        "added successfully!"
+        removedContact.name,
+        "deleted successfully!"
       );
-      return newContact;
-    })
-    .catch((err) => console.error(err.message));
+
+      return removedContact;
+    } else {
+      console.warn("\x1b[31m", "Contact with the specified ID not found");
+      return null;
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+addContact = async (name, email, phone) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+
+    const newContact = {
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    };
+
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+    console.table(newContact);
+    console.log("\x1b[32m", "Contact", newContact.name, "added successfully!");
+
+    return newContact;
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 module.exports = {
